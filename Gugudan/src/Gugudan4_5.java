83aa4585e2fc02142e6fcbf2c4b264beab696e47
ryudb <ryudb@DESKTOP-FLBB9CS>
2023-01-11 13:37:42 +0900

    따로 쓰지않고 한번에 출력

--- Gugudan/src/Gugudan4_5.java
+++ Gugudan/src/Gugudan4_5.java
@@ -8,23 +8,34 @@ public class Gugudan4_5 {
 		System.out.println("number : "+number);
 		
 		int result = number * 1;
-		System.out.println(result);
-		result = number * 2;
-		System.out.println(result);
-		result = number * 3;
-		System.out.println(result);
-		result = number * 4;
-		System.out.println(result);
-		result = number * 5;
-		System.out.println(result);
-		result = number * 6;
-		System.out.println(result);
-		result = number * 7;
-		System.out.println(result);
-		result = number * 8;
-		System.out.println(result);
-		result = number * 9;
-		System.out.println(result);
+//		System.out.println(result);
+//		result = number * 2;
+//		System.out.println(result);
+//		result = number * 3;
+//		System.out.println(result);
+//		result = number * 4;
+//		System.out.println(result);
+//		result = number * 5;
+//		System.out.println(result);
+//		result = number * 6;
+//		System.out.println(result);
+//		result = number * 7;
+//		System.out.println(result);
+//		result = number * 8;
+//		System.out.println(result);
+//		result = number * 9;
+//		System.out.println(result);
+		
+//		개쩌는 방법 따로 쓰지않고 한번에 합친다
+		System.out.println(number * 1);
+		System.out.println(number * 2);
+		System.out.println(number * 3);
+		System.out.println(number * 4);
+		System.out.println(number * 5);
+		System.out.println(number * 6);
+		System.out.println(number * 7);
+		System.out.println(number * 8);
+		System.out.println(number * 9);
 		
 		
 	}
