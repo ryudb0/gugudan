5c8101c52a14f3fb56d0b05f736fc9cfd9d80907
ryudb <ryudb@DESKTOP-FLBB9CS>
2023-01-11 12:18:08 +0900

    구구단

---
+++ Gugudan/src/Gugudan4_5.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Gugudan4_5 {
+	public static void main(String[] args) {
+		System.out.println("구구단 중 출력할 단은? : ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		System.out.println("number : "+number);
+		
+		int result = number * 1;
+		System.out.println(result);
+		result = number * 2;
+		System.out.println(result);
+		result = number * 3;
+		System.out.println(result);
+		result = number * 4;
+		System.out.println(result);
+		result = number * 5;
+		System.out.println(result);
+		result = number * 6;
+		System.out.println(result);
+		result = number * 7;
+		System.out.println(result);
+		result = number * 8;
+		System.out.println(result);
+		result = number * 9;
+		System.out.println(result);
+		
+		
+	}
+}
