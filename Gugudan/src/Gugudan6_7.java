f48da08bae4f557f9955faae1427ff5bce6942f6
ryudb <ryudb@DESKTOP-FLBB9CS>
2023-01-11 13:49:05 +0900

    구구단 while, for문사용

---
+++ Gugudan/src/Gugudan6_7.java
@@ -0,0 +1,17 @@
+
+public class Gugudan6_7 {
+	public static void main(String[] args) {
+//		6단
+		int i = 1;
+		while(i < 10) {
+			System.out.println("6 * "+i+" = "+ 6*i);
+			i = i+1;
+		}
+		
+//		7단
+		for(int j=1; j<10; j = j+1) {
+			System.out.println(7 * j);
+		}
+		
+	}
+}
